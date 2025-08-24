91e6b9bc10855c5b91bc82ca65df0452fd986e57
Arthur-Committer <arthur12.2007g@gmail.com>
2025-08-24 18:24:49 -0300

    Add Dynamic Array

---
+++ data-structures/dynamic_array.c
@@ -0,0 +1,64 @@
+#include "string.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define VECTOR_INLINE_CAPACITY 4
+
+typedef struct {
+  size_t size;
+  size_t capacity;
+  size_t elem_size;
+  void *data;
+  unsigned char buffer[VECTOR_INLINE_CAPACITY * sizeof(long long)];
+} Dynamic_Array;
+
+void vector_init(Vector *v, size_t elem_size) {
+  v->size = 0;
+  v->capacity = VECTOR_INLINE_CAPACITY;
+  v->elem_size = elem_size;
+  v->data = v->buffer;
+}
+
+void push_back(Vector *v, void *value) {
+  if (v->size == v->capacity) {
+    size_t new_capacity = v->capacity * 2;
+    void *new_data;
+    if (v->data == v->buffer) {
+      new_data = malloc(new_capacity * v->elem_size);
+      memcpy(new_data, v->data, v->size * v->elem_size);
+    } else {
+      new_data = realloc(v->data, new_capacity * v->elem_size);
+    }
+    if (!new_data)
+      exit(1);
+    v->data = new_data;
+    v->capacity = new_capacity;
+  }
+  memcpy((char *)v->data + v->size * v->elem_size, value, v->elem_size);
+  ++v->size;
+}
+
+void vector_free(Vector *v) {
+  if (v->data != v->buffer) {
+    free(v->data);
+  }
+  v->data = NULL;
+  v->size = v->capacity = 0;
+}
+#define VECTOR_AT(v, type, i) (((type *)(v).data)[(i)])
+int main() {
+  Vector v;
+  vector_init(&v, sizeof(char));
+  char num = 0;
+entry:
+  scanf("%c", &num);
+  if (num != 'p') {
+    push_back(&v, &num);
+    goto entry;
+  }
+
+  for (size_t i = 0; i < v.size; ++i) {
+    printf("%c", VECTOR_AT(v, char, i));
+  }
+}
---
+++ data-structures/src/main.c
@@ -0,0 +1,20 @@
+
+// app.c
+#include "dynamic_array.h"
+#include <assert.h>
+#include <stdio.h>
+
+int main(void) {
+  DynamicArray *a = da_create(sizeof(char));
+  assert(a);
+
+  const char *msg = "abcde";
+  for (const char *p = msg; *p; ++p) {
+    int st = da_push_back(a, p);
+    assert(st == DYN_OK);
+  }
+
+  fwrite(da_cdata(a), da_elem_size(a), da_size(a), stdout); // imprime abcde
+  da_destroy(a);
+  return 0;
+}
