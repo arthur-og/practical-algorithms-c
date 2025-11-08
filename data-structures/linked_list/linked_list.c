338f9df84f495576ad0d06e7fcd6e97989581497
Arthur-Committer <arthur12.2007g@gmail.com>
2025-11-08 16:07:40 -0300

    add inverter list,and improve generlization

--- data-structures/linked_list/linked_list.c
+++ data-structures/linked_list/linked_list.c
@@ -2,14 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
-void print_int(void *data) { printf("        %d      ", *(int *)data); }
-void print_float(void *data) { printf("    %f   ", *(float *)data); }
-void print_string(void *data) { printf("    %s    ", (char *)data); }
+void print_int(void *data, int len) {
+  printf("       %d       ", *(int *)data);
+}
+void print_float(void *data, int len) { printf("     %f    ", *(float *)data); }
+void print_string(void *data, int len) { printf("    %s   ", (char *)data); }
+
+void print_int_arr(void *data, int len) {
+
+  int count = len / sizeof(int);
+  printf("    [");
+  for (int i = 0; i < count; ++i) {
+    printf(" %d ", ((int *)data)[i]);
+  }
+  printf("]     ");
+}
+
+typedef void (*PrintFunc)(void *data, int len);
 
 typedef struct Node {
   void *data;
-  struct Node *Next;
-  void (*print_pointer)(void *data);
+  int data_len;
+  struct Node *next;
+  PrintFunc print_pointer;
 } Node;
 
 typedef struct {
@@ -18,64 +33,162 @@ typedef struct {
   int size;
 } LinkedList;
 
-LinkedList *new(void *dt, int len, void (*print_func)(void *data)) {
-  LinkedList *list = (LinkedList *)malloc(sizeof(LinkedList));
-  Node *no = (Node *)malloc(sizeof(Node));
-  no->data = malloc(len);
-  no->Next = NULL;
-  no->print_pointer = print_func;
-  memcpy(no->data, dt, len);
+Node *create_node(void *dt, int len, PrintFunc print_func) {
 
-  list->head = no;
-  list->tail = no;
-  list->size = 1;
+  Node *node = (Node *)malloc(sizeof(Node));
+  if (node == NULL) {
+    printf("node aloccation fail");
+    return NULL;
+  }
+
+  node->data = malloc(len);
+  if (node->data == NULL) {
+    printf("data node aloccation fail");
+    free(node);
+    return NULL;
+  }
+
+  memcpy(node->data, dt, len);
+  node->data_len = len;
+  node->next = NULL;
+  node->print_pointer = print_func;
+  return node;
+}
+
+LinkedList *create_list() {
+  LinkedList *list = (LinkedList *)malloc(sizeof(LinkedList));
+  if (list == NULL) {
+    printf("list aloccation fail");
+    return NULL;
+  }
+  list->head = NULL;
+  list->tail = NULL;
+  list->size = 0;
   return list;
 }
 
-void add_tail(LinkedList *list, void *dt, int len,
-              void (*print_func)(void *data)) {
-  Node *no = (Node *)malloc(sizeof(Node));
-  no->data = malloc(len);
-  memcpy(no->data, dt, len);
-  list->tail->Next = no;
-  list->tail = no;
-  no->Next = NULL;
+void list_append(LinkedList *list, void *dt, int len, PrintFunc print_func) {
+  Node *node = create_node(dt, len, print_func);
+  if (node == NULL)
+    return;
 
-  no->print_pointer = print_func;
-
+  if (list->size == 0) {
+    list->head = node;
+    list->tail = node;
+  } else {
+    list->tail->next = node;
+    list->tail = node;
+  }
   list->size++;
 }
 
-void generic_print(LinkedList *list) {
-  Node *no = list->head;
-  for (int i = 1; i <= list->size - 1; ++i) {
-    no->print_pointer(no->data);
-    printf("-->");
-    no = no->Next;
+void list_print(void *lista, int len) {
+  LinkedList *list = (LinkedList *)lista;
+  if (list->head == NULL) {
+    printf("List is empty");
+    return;
   }
-  no->print_pointer(no->data);
-  putchar('\n');
+  putchar('|');
+  Node *current = list->head;
+  while (current != NULL) {
+    current->print_pointer(current->data, current->data_len);
+    if (current->next != NULL) {
+      printf("  -->");
+    }
 
-  no = list->head;
-  for (int i = 1; i <= list->size - 1; ++i) {
-    printf(" %p -->", no->Next);
-    no = no->Next;
+    current = current->next;
   }
-  printf("       %p    ", no->Next);
-
-  putchar('\n');
+  // putchar('\n');
+  /*
+    current = list->head;
+    while (current != NULL) {
+      printf("  %p  ", (void *)current->next);
+      if (current->next != NULL) {
+        printf(" -->");
+      }
+      current = current->next;
+    }
+    */
+  putchar(' ');
+  putchar('|');
+  // putchar('\n');
 };
 
+void list_inverter(LinkedList *list) {
+  Node *prev = list->head;
+  Node *current = list->head->next;
+  list->head->next = NULL;
+  Node *next_node;
+
+  while (current != NULL) {
+    next_node = current->next;
+    current->next = prev;
+    prev = current;
+    current = next_node;
+  }
+  Node *temp = list->head;
+  list->head = list->tail;
+  list->tail = temp;
+}
+
+void free_list(LinkedList *list) {
+  if (list == NULL)
+    return;
+
+  Node *current = list->head;
+  Node *next_node;
+
+  while (current != NULL) {
+    next_node = current->next;
+    free(current->data);
+    free(current);
+    current = next_node;
+  }
+  free(list);
+}
+
+int list_len(LinkedList *list) {
+  if (list == NULL)
+    return -1;
+
+  Node *current = list->head;
+  Node *next_node;
+  int len = 0;
+
+  while (current != NULL) {
+    next_node = current->next;
+    len += current->data_len;
+    current = next_node;
+  }
+
+  return len;
+}
+
 int main() {
-  int valor1 = 12;
-  float valor2 = 12.123;
-  char valor3[] = "ai dento";
-  float valor4 = 129.78;
+  int value1 = 12;
+  float value2 = 12.123;
+  char str1[] = "star wars";
+  float value4 = 129.78;
+  int arr[] = {1, 12, 5, 6, 9};
+  char str2[] = "horcrux";
 
-  LinkedList *list = new (&valor1, sizeof(valor1), print_int);
+  LinkedList *list = create_list();
 
-  add_tail(list, &valor2, sizeof(valor2), print_float);
-  add_tail(list, &valor3, sizeof(valor3), print_string);
-  add_tail(list, &valor4, sizeof(valor4), print_float);
-  generic_print(list);
+  list_append(list, &value1, sizeof(value1), print_int);
+  list_append(list, &value2, sizeof(value2), print_float);
+
+  LinkedList *list2 = create_list();
+
+  list_append(list2, &str1, sizeof(str1), print_string);
+  list_append(list2, &value4, sizeof(value4), print_float);
+  list_append(list2, arr, sizeof(arr), print_int_arr);
+  list_append(list, list2, list_len(list2), list_print);
+
+  list_append(list, str2, sizeof(str2), print_string);
+
+  list_print(list, 0);
+  putchar('\n');
+  // list_inverter(list);
+  //  printf("inverted:\n");
+  //  list_print(list, 2);
 }
